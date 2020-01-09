bc93c83c6ddb3f91f9a46cc0446977a488bb7d95
Lula Freire <luizalbertofreire@gmail.com>
2020-01-08 21:34:35 -0300

    08-01-20

---
+++ index.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <style type="text/css">
+      .fixedHeightImg > img {
+        max-height: 500px;
+        margin-left: auto;
+        margin-right: auto;
+        width: auto;
+      }
+
+      .carousel-caption {
+          background-color: rgb(0,0,0,0.5);
+      }
+
+      .w900 {
+        font-weight: bolder;
+      }
+
+      .exist {
+          text-shadow: 0.4px 0px 0px, 
+                      -0.4px 0px 0px, 
+                      0px 0.4px 0px, 
+                      0px -0.4px 0px;          
+          font-weight: bold;         
+      }
+
+      .rodape {
+        line-height: 2em;
+      }  
+
+    </style>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/4a5c9625c6.js" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background: #907835;">
+
+      <div class="container">
+
+        <a class="navbar-brand h1 mb-0" href="#">ENTR<b>&exist;</b>LINHAS</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSite">
+
+          <ul class="navbar-nav">
+            
+            <li class="nav-item">
+              <a class="nav-link" href="#">Início</a>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Categorias</a>
+
+              <div class="dropdown-menu">
+                <a href="#" class="dropdown-item">Política e Sociedade</a>
+                <a href="#" class="dropdown-item">Arte e Cultura</a>
+                <a href="#" class="dropdown-item">Mundo do Trabalho</a>
+                <a href="#" class="dropdown-item">Ciência e Tecnologia</a>
+              </div>
+
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Canais</a>
+
+              <div class="dropdown-menu">
+                <a href="#" class="dropdown-item"><i class="fab fa-youtube"></i> YouTube</a>
+                <a href="#" class="dropdown-item"><i class="fas fa-microphone-alt"></i> Podcasts</a>
+                <a href="#" class="dropdown-item"><i class="fab fa-twitter"></i> Twitter</a>
+                <a href="#" class="dropdown-item"><i class="fab fa-facebook-square"></i> Facebook</a>
+                <a href="#" class="dropdown-item"><i class="fab fa-instagram"></i> Instagram</a>
+              </div>
+
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Sobre</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contato</a>
+            </li>
+
+          </ul>
+
+          <form class="form-inline ml-auto">
+            <input class="form-control mr-2" type="search" placeholder="Buscar" name="busca">
+            <button class="btn btn-dark" type="submit">Ok</button>
+          </form>
+
+        </div>
+        
+      </div>      
+      
+    </nav>
+
+    <!-Carousel-->
+
+    <div class="max-"></div>
+
+    <div class="carousel slide" id="carouselSite" data-ride="carousel">
+
+      <ol class="carousel-indicators">
+        <li data-target="#carouselSite" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselSite" data-slide-to="1"></li>
+        <li data-target="#carouselSite" data-slide-to="2"></li>
+      </ol>
+
+      <div class="carousel-inner">
+
+        <div class="carousel-item active fixedHeightImg">
+
+          <img src="assets/images/bernauer-strae-113173_640.jpg" class="image-fluid d-block w-100" style="height: 500px;">
+
+          <div class="carousel-caption d-none d-md-block">
+
+            <h3>EDUCAÇÃO</h3>
+            <h5>As quotas reduziram a desigualdade nas Universidades</h5>
+            
+          </div>
+          
+        </div>
+
+        <div class="carousel-item fixedHeightImg">
+
+          <img src="assets/images/fallow-deer-984573_640.jpg" class="image-fluid d-block w-100" style="height: 500px;">
+
+          <div class="carousel-caption d-none d-md-block">
+
+            <h3>MEIO AMBIENTE</h3>
+            <h5>Como o neoliberalismo está destruindo as reservas naturais</h5>
+            
+          </div>
+          
+        </div>
+
+        <div class="carousel-item fixedHeightImg">
+          
+          <img src="assets/images/working-1024382_640.jpg" class="image-fluid d-block w-100" style="height: 500px;">
+
+          <div class="carousel-caption d-none d-md-block">
+
+            <h3>MUNDO DO TRABALHO</h3>
+            <h5>Subemprego alimenta estatísticas do governo sobre desemprego</h5>
+            
+          </div>
+
+        </div>
+
+        <a class="carousel-control-prev" href="#carouselSite" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselSite" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Próximo</span>
+        </a>
+        
+      </div>
+      
+    </div>
+
+    <!-- Editorial -->
+    <div class="container mt-4">
+
+      <div>
+        <h5><strong>EDITORIAL</strong> <small><i><font class="text-muted">por Lula Freire</font></i></small></h5>
+        <hr size="1" width="100%" class="text-secondary mb-4">
+      </div>
+
+      <div class="row">
+
+        <div class="col-6">
+
+          <h1>Eleições 2018: Uma escolha nada difícil</h1>
+          <p class="lead">Como a grande mídia criou uma falsa polarização entre Haddad e Bolsonaro</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum, magna sed bibendum mollis, sem eros tempus arcu, non pulvinar leo sapien placerat ex. Donec suscipit metus dolor, et dictum dui faucibus quis. Pellentesque libero lacus, egestas sit amet elit at, bibendum luctus ligula. Praesent suscipit aliquet lacus, eget aliquam lorem. Integer sit amet hendrerit erat, nec maximus felis. Vivamus mattis ullamcorper orci. Fusce auctor, mi eu scelerisque tempor, massa enim efficitur purus, eu facilisis dui odio vitae lacus. Praesent tellus est, cursus ut magna at, pretium tempus nulla. Integer pellentesque tristique purus, at consequat erat molestie eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </p>
+          
+        </div>
+
+        <div class="col-6">
+
+          <img src="assets/images/newspapers-4565916_640.jpg" class="w-100">
+          
+        </div>
+        
+      </div>
+
+      <hr size="1" width="100%" class="text-secondary mb-2">
+
+      <div class="row justify-content-sm-center">
+
+        <div class="col-sm-6 col-md-4">
+
+          <div class="card my-4">
+
+            <div class="card-header">
+              <h6>Cultura e Arte</h6>
+            </div>
+
+            <img src="assets/images/newspapers-4565916_640.jpg" class="card-img-top">
+
+            <div class="card-body">
+
+              <h4 class="card-title"><strong>Pellentesque habitant morbi eget aliquam lorem</strong></h4>
+              <h6 class="card-subtitle text-muted mb-3">Por Osvaldo de Oliveira</h6>
+
+              <p class="card-text">Vivamus mattis ullamcorper orci. Fusce auctor, mi eu scelerisque tempor, massa enim efficitur purus, eu facilisis dui odio vitae lacus.</p>
+
+              <a href="#" class="card-link">Leia mais</a>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+        
+        <div class="col-sm-6 col-md-4">
+
+          <div class="card my-4">
+            
+            <div class="card-header">
+              <h6>Mundo do Trabalho</h6>
+            </div>
+
+            <img src="assets/images/working-1024382_640.jpg" class="card-img-top">
+
+            <div class="card-body">
+
+              <h4 class="card-title"><strong>Pellentesque habitant morbi eget aliquam lorem</strong></h4>
+              <h6 class="card-subtitle text-muted mb-3">Por Osvaldo de Oliveira</h6>
+
+              <p class="card-text">Vivamus mattis ullamcorper orci. Fusce auctor, mi eu scelerisque tempor, massa enim efficitur purus, eu facilisis dui odio vitae lacus.</p>
+
+              <a href="#" class="card-link">Leia mais</a>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+
+          <div class="card my-4">
+
+            <div class="card-header">
+              <h6>Política e Sociedade</h6>
+            </div>
+
+            <img src="assets/images/old-letters-1082299_640.jpg" class="card-img-top">
+
+            <div class="card-body">
+
+              <h4 class="card-title"><strong>Pellentesque habitant morbi eget aliquam lorem</strong></h4>
+              <h6 class="card-subtitle text-muted mb-3">Por Osvaldo de Oliveira</h6>
+
+              <p class="card-text">Vivamus mattis ullamcorper orci. Fusce auctor, mi eu scelerisque tempor, massa enim efficitur purus, eu facilisis dui odio vitae lacus.</p>
+
+              <a href="#" class="card-link">Leia mais</a>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+
+      </div>
+      
+    </div>
+
+    <!-- Multimídia -->
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <div class="col-12 text-center">
+          <h1 class="disply-4">ENTR<font class='exist'>&exist;</font>LINHAS<i><small>Multimídia</small></i></h1>            
+          <hr>
+        </div>
+
+        <div class="col-12">
+          <ul class="nav nav-pills justify-content-center mb-4" role="tablist" id="pills-nav">
+            <li class="nav-item">
+              <a class="nav-link active" id="nav-pills-01" data-toggle="pill" href="#nav-item-01">Vídeos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="nav-pills-02" data-toggle="pill" href="#nav-item-02">Podcasts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="nav-pills-03" data-toggle="pill" href="#nav-item-03">Fotos</a>
+            </li>         
+          </ul>
+
+          <div class="tab-content" id="nav-pills-content">
+            <div class="tab-pane fade show active" id="nav-item-01" role="tabpanel">
+              <div class="row">
+                <div class="col-6">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/FCpcvb2vpGk" allowfullscreen></iframe>
+                  </div>
+                </div>
+                <div class="col-6">
+                  <h4 class="display-5">Como as democracias morrem</h4>
+                  <h6 class="lead mb-3">Resenha coletiva do livro de Steven Levitsky e Daniel Ziblatt</h6>
+                  <p>Uma análise crua e perturbadora do fim das democracias em todo o mundo Democracias tradicionais entram em colapso? Essa é a questão que Steven Levitsky e Daniel Ziblatt - dois conceituados professores de Harvard - respondem ao discutir o modo como a eleição de Donald Trump se tornou possível. Para isso comparam o caso de Trump com exemplos históricos de rompimento da democracia nos últimos cem anos: da ascensão de Hitler e Mussolini nos anos 1930 à atual onda populista de extrema-direita na Europa, passando pelas ditaduras militares da América Latina dos anos 1970.</p>
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane fade show" id="nav-item-02" role="tabpanel">
+              <div class="row">
+                <div class="col-6">
+                  <img class="w-100" src="assets/images/akarneiro.jpg" alt="">
+                </div>
+                <div class="col-6">
+                <h4 class="display-5">Um sertanejo apaixonado</h4>
+                  <h6 class="lead mb-3">Como o artista AKarneiro está construindo o Memorial do Sertão</h6>
+                  <p>Versão em áudio da entrevista com o artista plástico ireceense AKarneiro.</p>
+                  <audio controls>
+                    <source src="assets/podcasts/teste.mp3" type="audio/mpeg">                    
+                    Seu navegador não suporta áudio tag.
+                </audio>                
+                </div>
+              </div>
+            </div>
+            <div class="tab-pane fade show" id="nav-item-03" role="tabpanel">
+              <div class="row">
+                <div class="col-6">
+                  <img class="w-100" src="assets/images/flicop.jpg" alt="">
+                </div>
+                <div class="col-6">
+                <h4 class="display-5">FLICOP - Feira Literária da Coperil</h4>
+                  <h6 class="lead mb-3">Cooperativa Educacional promove importante feira de livros em Irecê</h6>
+                  <p>Confira os registros fotográficos da II Flicop.</p>                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div>
+        <h5><strong>LER, ASSISTIR, OUVIR</strong> <small><i><font class="text-muted">Sugestões de livros, filmes e músicas dos editores</font></i></small></h5>
+        <hr size="1" width="100%" class="text-secondary mb-4">
+      </div>
+
+      <div class="row my-4">
+        <div class="col-8">
+          <h4>A revolução dos bichos, de George Orwell</h4>
+          <h6 class="lead mb-4">A fábula que foi publicada em 1945 permanece atual até os dias de hoje</h6>
+          <p>Em tempos politicamente cada vez mais conturbados, vale ler novamente A revolução dos bichos, um clássico escrito por George Orwell. A fábula que foi publicada em 1945 permanece atual até os dias de hoje e a sua leitura nos faz embarcar em um mundo paralelo que, ao contrário do que possa parecer a primeira vista, tem muito a ver com o nosso.<br>
+          A história se passa na Granja do Solar, onde uma série de bichos se reúne para fazer uma revolução. São galinhas, pombas, cachorros, cavalos, vacas, cabras, burros e ovelhas que possuem características humanas.<br>
+          Liderados pelos porcos - considerados os mais inteligentes do grupo -, os animais desejam dar fim à exploração feita pelo senhor Jones, o dono da quinta. Com muito custo o proprietário acaba por ser expulso da sua quinta e os animais tomam a frente da quinta, criando um novo tipo de sociedade, igualitária e baseada na não exploração.<br>
+          A leitura de A revolução dos bichos é repleta de humor e nos faz pensar sobre a nossa própria sociedade, o sistema de trabalho, as condições de vida, a desigualdade.</p>
+        </div>
+        <div class="col-4">
+          <img class="w-100" src="assets/images/a-revolucao-dos-bichos-de-george-orwell-cke.jpg" alt="">
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid" style="background: #907835; height: 570px;">
+      <div class="container">
+        <div class="row">
+          <div class="col-4 my-5 jystify-content-center">
+            <img class="w-50" src="assets/images/logo.png" alt="">
+          </div>
+          <div class="col-4 my-5">
+            <h6 class="text-light"><strong><u>SOBRE O PROJETO</u></strong></h6>
+            <p class="display-6 text-light rodape">Entrelinhas é um projeto coletivo de contextualização crítica dos fatos históricos e das produções multimodais sobre o cotidiano.</p>
+            <p class="display-6 text-light rodape">O principal objetivo é divulgar textos, vídeos e áudios com resenhas e reflexões de diversas linhas progressitas de pensamento.</p>
+            <h6 class="text-light my-4"><strong><u>CANAIS</u></strong></h6>
+            <i class="fab fa-facebook-square text-light fa-3x"></i>&nbsp;&nbsp;&nbsp;
+            <i class="fab fa-instagram text-light fa-3x"></i>&nbsp;&nbsp;&nbsp;
+            <i class="fab fa-twitter-square text-light fa-3x"></i>&nbsp;&nbsp;&nbsp;
+            <i class="fas fa-podcast text-light fa-3x"></i>
+          </div>
+          <div class="col-4 my-5">
+            <h6 class="text-light"><strong><u>CATEGORIAS</u></strong></h6>
+            <p class="text-light rodape">
+            POLÍTICA E CIDADANIA<br>
+            CULTURA E ARTE<br>
+            MUNDO DO TRABALHO<br>
+            CIÊNCIA E TECNOLOGIA</p>
+          </div>          
+        </div>
+        <hr class="text-light">
+        <div class="col-12 mt-4">
+          <blockquote class="blockquote text-center text-light">
+            <p class="mb-0">Imagine todas as pessoas compartilhando o mundo inteiro.</p>
+            <footer class="blockquote-footer text-light">John Lennon <cite title="Título da fonte">na música Imagine</cite></footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/popper.js"></script>
+  </body>
+</html>
